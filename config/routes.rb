a26c702b30a36d16ae249841c636b48a5e4ef2b1
Ibitoye Ayanfeoluwa <ibitoyeayanfeoluwa@yahoo.com>
2015-08-14 17:12:45 +0000

    undo again

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,5 @@ Rails.application.routes.draw do
   get 'about'   => 'pages#about'
   get 'contact' => 'pages#contact'
   get 'signup'  => 'users#new'
-  get    'login'   => 'sessions#new'
-  post   'login'   => 'sessions#create'
-  delete 'logout'  => 'sessions#destroy'
   resources :users
 end
