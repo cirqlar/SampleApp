0ba9b1f0e5ba140db76b14430a4c45866afbadff
Ibitoye Ayanfeoluwa <ibitoyeayanfeoluwa@yahoo.com>
2015-08-14 19:46:01 +0000

    lilo-Generate controller Session new and modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
   get 'about'   => 'pages#about'
   get 'contact' => 'pages#contact'
   get 'signup'  => 'users#new'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
   resources :users
 end
