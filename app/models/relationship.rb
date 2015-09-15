168664a1d9665f0d902718621db85523cce937f6
Ayanfeoluwa Ibitoye <ibitoyeayanfeoluwa@yahoo.com>
2015-09-15 19:32:17 +0000

    relationship.rb edited online with Bitbucket

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,9 +1,6 @@
 class Relationship < ActiveRecord::Base
-<<<<<<< HEAD
-=======
-	belongs_to :follower, class_name: "User"
+  belongs_to :follower, class_name: "User"
   belongs_to :followed, class_name: "User"
   validates :follower_id, presence: true
   validates :followed_id, presence: true
->>>>>>> 4eed39701cb0b9b37958af8f4bc74bd8c553dbb8
 end
